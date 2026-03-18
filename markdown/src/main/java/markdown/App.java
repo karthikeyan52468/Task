91c6999dc1df966b70ef37250bc7ba9728537e8f
karthikeyan52468 <karthikeyan60780@gmail.com>
2026-03-18 12:54:16 +0530

    h1 p ol br img href in markdown

--- markdown/src/main/java/markdown/App.java
+++ markdown/src/main/java/markdown/App.java
@@ -8,12 +8,18 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		String s="# welcome \n \n test paragaraph \n"
+				+ "\n 1. bmw \n"
+				+ "2. audi \n \n"
+				+ "[click](www.example.com)\n"
+				+ "___\n"
+				+ "![error](www.example.com)";
 		Parser parse = org.commonmark.parser.Parser.builder().build();
 
-		Node document = parse.parse("# hello world");
+		Node document = parse.parse(s);
 		HtmlRenderer render = HtmlRenderer.builder().build();
-		String s=render.render(document);
-		System.out.println(s);
+		String html=render.render(document);
+		System.out.println(html);
 	}
 
 }
